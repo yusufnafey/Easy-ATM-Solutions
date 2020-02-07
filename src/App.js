b2958d61ee55964b116b4f9708a34b6c6fbcff16
Yusuf <yusufnafey@yahoo.com>
2020-02-06 19:20:24 -0800

    adds reactstrap, under construction, navbar, footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import './App.scss';
 import './App.css';
+import Navbar from './Navbar'
+import Footer from './Footer'
+import { Alert, Button } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div class="under-construction-container">
+        <h1 class="under-construction-title">🚧UNDER CONSTRUCTION🚧</h1>
+        <h4 class="under-construction-subtitle">(but feel free to browse, ignore the mess 😅)</h4>
+      </div>
+
+        <Navbar />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+Easy ATM Solutions
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Footer />
+        <Alert color="primary">
+  This is a primary Alert — check it out!
+</Alert>
+        <Button color="primary">
+  This is a primary button — check it out!
+</Button>
       </header>
     </div>
   );
